9173cee4c95c03266cf98e9c894d6bbbf6d4b391
Madan, Smarth <smadan@paypal.com>
2016-06-01 17:43:33 -0700

    Write/read testing for DB size

--- src/test/java/com/xjeffrose/chicago/loadTest/ChicagoLoadTest.java
+++ src/test/java/com/xjeffrose/chicago/loadTest/ChicagoLoadTest.java
@@ -16,45 +16,54 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class ChicagoLoadTest {
-  static ChicagoTSClient chicagoClientDHT;
+  static ChicagoClient chicagoClientDHT;
 
   @BeforeClass
   static public void setupFixture() throws Exception {
-    chicagoClientDHT = new ChicagoTSClient("10.24.25.188:2181,10.24.25.189:2181,10.25.145.56:2181,10.24.33.123:2181", 3);
+    chicagoClientDHT = new ChicagoClient("127.0.0.1:2181", 1);
     //chicagoClientDHT = new ChicagoClient("10.22.100.183:2181/chicago");
   }
 
   @Test
   public void writeMany() throws Exception {
-    String _k = "smkey";
-    byte[] key = _k.getBytes();
-    for (int i = 0; i < 1000; i++) {
-      String _v = "val" + i;
-      byte[] val = _v.getBytes();
-      assertNotNull(chicagoClientDHT.write(key, val));
+    StringBuilder _v = new StringBuilder("");
+    for (int i = 1; i < 1000000; i++) {
+      String _k = "key" + i;
+      byte[] key = _k.getBytes();
+      if(i%1000 ==0){
+        _v.delete(0,_v.length()-1);
+      }
+      _v.append("val" + i);
+      byte[] val = _v.toString().getBytes();
+      assertEquals(true,chicagoClientDHT.write(key, val));
+      //assertEquals(new String(val), new String(chicagoClientDHT.read(key).get()));
       System.out.println(i);
     }
   }
 
+  //@Test
+  //public void readStream() throws Exception {
+  //  ListenableFuture<ChicagoStream> f = chicagoClientDHT.stream("smkey".getBytes());
+  //  ChicagoStream cs = f.get(1000, TimeUnit.MILLISECONDS);
+  //  ListenableFuture<byte[]> resp = cs.getStream();
+  //
+  //  System.out.println(new String(resp.get(1000, TimeUnit.MILLISECONDS)));
+  //}
+
   @Test
-  public void readStream() throws Exception {
-    ListenableFuture<ChicagoStream> f = chicagoClientDHT.stream("smkey".getBytes());
-    ChicagoStream cs = f.get(1000, TimeUnit.MILLISECONDS);
-    ListenableFuture<byte[]> resp = cs.getStream();
-
-    System.out.println(new String(resp.get(1000, TimeUnit.MILLISECONDS)));
+  public void readMany() throws Exception {
+    StringBuilder _v = new StringBuilder("");
+    for (int i = 98001; i < 99000; i++) {
+      String _k = "key" + i;
+      byte[] key = _k.getBytes();
+      if(i%1000 ==0){
+        _v.delete(0,_v.length()-1);
+      }
+      _v.append("val" + i);
+      byte[] val = _v.toString().getBytes();
+      assertEquals(_v.toString(),new String(chicagoClientDHT.read(key).get()));
+    }
   }
-
-//  @Test
-//  public void readMany() throws Exception {
-//    for (int i = 0; i < 100; i++) {
-//      String _k = "key" + i;
-//      byte[] key = _k.getBytes();
-//      String _v = "val" + i;
-//      byte[] val = _v.getBytes();
-//      assertEquals(new String(val), new String(chicagoClientDHT.read(key).get()));
-//    }
-//  }
 //
 //    @Test
 //    public void deleteMany() throws Exception {
