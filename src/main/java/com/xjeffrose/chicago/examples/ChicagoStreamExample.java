a9ae7eca6bcea7e31682e49204d27b3f4039d72b
Smarth Madan <smadan@paypal.com>
2016-06-27 13:47:02 -0700

    Adding few examples

--- src/main/java/com/xjeffrose/chicago/examples/ChicagoStreamExample.java
+++ src/main/java/com/xjeffrose/chicago/examples/ChicagoStreamExample.java
@@ -1,6 +1,8 @@
 package com.xjeffrose.chicago.examples;
 
+import com.google.common.primitives.Ints;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.xjeffrose.chicago.ChiUtil;
 import com.xjeffrose.chicago.client.ChicagoStream;
 import com.xjeffrose.chicago.client.ChicagoTSClient;
 
@@ -17,40 +19,51 @@ public class ChicagoStreamExample {
     cs.chicagoTSClient = new ChicagoTSClient("10.24.25.188:2181,10.24.25.189:2181,10.25.145.56:2181,10.24.33.123:2181",3);
     cs.chicagoTSClient.startAndWaitForNodes(3);
     cs.transactStream();
+    System.exit(0);
   }
 
 
   public void transactStream() throws Exception {
-    String key = "ppfe";
-    byte[] offset = null;
+    String key = "writeTestKey";
+    int offset = -1;
 
     ListenableFuture<com.xjeffrose.chicago.client.ChicagoStream> f = chicagoTSClient.stream(key.getBytes());
     com.xjeffrose.chicago.client.ChicagoStream cs = f.get();
     ListenableFuture<byte[]> resp = cs.getStream();
 
-    String result = new String(resp.get());
+    byte[] resultArray = resp.get();
+    String result = new String(resultArray);
     cs.close();
-    byte[] old=null;
-    int count =0;
+    int old=-1;
     while(true){
-      if(!result.contains("@@@"))
+      if(!result.contains(ChiUtil.delimiter)){
+        System.out.println("No delimetr present");
+        System.out.println(result);
         break;
-      offset = result.split("@@@")[1].getBytes();
-      String[] lines = (result.split("@@@")[0]).split("\0");
+
+      }
+
+      offset = ChiUtil.findOffset(resultArray);
+      String[] lines = (result.split(ChiUtil.delimiter)[0]).split("\0");
+      int count =0;
       for(String line : lines){
         if(line.length()!= 0) {
+          System.out.println(offset +":" +line);
           count++;
-          System.out.println(count +" : "+line);
         }
       }
-      if(old != null && Arrays.equals(old,offset)){
+      if(count > 0){
+        offset = offset + 1;
+      }
+      if(old != -1 && (old == offset)){
         Thread.sleep(500);
       }
 
-      ListenableFuture<com.xjeffrose.chicago.client.ChicagoStream> _f = chicagoTSClient.stream(key.getBytes(), offset);
+      ListenableFuture<com.xjeffrose.chicago.client.ChicagoStream> _f = chicagoTSClient.stream(key.getBytes(), Ints.toByteArray(offset));
       ChicagoStream newcs = _f.get();
       ListenableFuture<byte[]> newresp = newcs.getStream();
-      result = new String(newresp.get());
+      resultArray = newresp.get();
+      result = new String(resultArray);
       old = offset;
     }
 
