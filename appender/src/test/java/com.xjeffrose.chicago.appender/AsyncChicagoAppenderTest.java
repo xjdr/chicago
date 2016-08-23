e8cee3a4f3a5c191557c8762bfd4a9169e1bb877
Madan, Smarth <smadan@paypal.com>
2016-08-22 19:41:21 -0700

    Fixing startup time failure mode

---
+++ appender/src/test/java/com.xjeffrose.chicago.appender/AsyncChicagoAppenderTest.java
@@ -0,0 +1,21 @@
+package com.xjeffrose.chicago.appender;
+
+import org.junit.Test;
+
+/**
+ * Created by smadan on 8/22/16.
+ */
+public class AsyncChicagoAppenderTest {
+
+  @Test
+  public void testAppenderNoServer(){
+    AsyncChicagoAppender chicagoAppender = new AsyncChicagoAppender();
+    chicagoAppender.setChicagoZk("junkIP:2181");
+    chicagoAppender.setKey("TestKey");
+    try {
+      chicagoAppender.activateOptions();
+    }catch (Exception e){
+      e.printStackTrace();
+    }
+  }
+}
